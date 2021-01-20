ae197d9e5ef428821762f294c66e317f4a861ad7
Rodrigo Ramirez <rodrigo8rb@gmail.com>
2021-01-19 22:25:42 -0500

    ⚡ Updated Token
    Updated token location on a separate file

---
+++ src/assets/token.js
@@ -0,0 +1,3 @@
+var token = 'BQDM_qJtK15wlxpBvOHyRRrrzL_RfmbpqoAeZaLttGrRKGSR75hI1dba5AYZ8aaiCPrcox9CTnTJ_r3TfJF_77Oe-culxkDRuE-gESKlqmSugGFmP57C72IdPZoctYugTT8MzZCEaK0cUk5kXYOgU-GVz7ida44tWSJs-Xg-0AeU-Tb9-m6h3ogVuW8f0ePpV_1lhN7Rxu6O8uhAlbF02asYWQGOc8RgEggsTbYblkIX9a3TWX6bKbc6TlM-O3pDcQTwJ9w4qzQ-_rj-Bsf1wZiMvZS6mcLtxa7O51c';
+
+export default token;

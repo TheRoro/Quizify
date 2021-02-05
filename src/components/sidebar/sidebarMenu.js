501ccfe08b9c864441329aa1266183bbf67df03d
Camila Martinez Bedoya <camilamarb98@gmail.com>
2021-02-05 16:06:52 -0500

    added sidebar menu

---
+++ src/components/sidebar/sidebarMenu.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export const SidebarMenu = [
+  {
+    title: "Quizify",
+    path: "/",
+    cName: "quizify-title",
+  },
+  {
+    title: "Lorem Ipsum",
+    cName: "menu-item",
+  },
+  {
+    title: "Lorem Ipsum",
+    cName: "menu-item",
+  },
+  {
+    title: "Copyright",
+    cName: "copyright",
+  },
+];

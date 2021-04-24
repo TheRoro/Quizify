68a0a0ae1542a8f2d7cdca931bade19bf833239f
Rodrigo Ramirez <rodrigo8rb@gmail.com>
2021-04-24 16:25:43 -0500

    🎨 Navbar Updates
    Fix navbar items alignment and close on click. 🙈

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -17,20 +17,25 @@ function Sidebar() {
         </div>
       </div>
       <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
-        <ul className="nav-menu-items" onClick={showSidebar}>
-          <li className="navbar-toggle">
-            <div className="menu-bars">
-              <HiIcons.HiX />
-            </div>
-          </li>
-          {SidebarMenu.map((item, index) => {
-            return (
-              <li key={index} className={item.cName}>
-                <Link to={item.path}>{item.title}</Link>
-              </li>
-            );
-          })}
-        </ul>
+        <nav className="left-nav" onClick={showSidebar}>
+          {/* EL REGRESO */}
+        </nav>
+        <nav className="right-nav">
+          <ul className="nav-menu-items" onClick={showSidebar}>
+            <li className="navbar-toggle">
+              <div className="menu-bars">
+                <HiIcons.HiX />
+              </div>
+            </li>
+            {SidebarMenu.map((item, index) => {
+              return (
+                <li key={index} className={item.cName}>
+                  <Link to={item.path}>{item.title}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
       </nav>
     </>
   );

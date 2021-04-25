bf58c4ee2c07128b3747c882fea92d4683bc8118
Camila Martinez <camilamarb98@gmail.com>
2021-04-24 20:31:42 -0500

    add and style copyright

---
+++ src/components/copyright/copyright.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Rodrigo from "../../assets/images/roro.jpg";
+import Camila from "../../assets/images/camila.jpg";
+import "./../home/home.scss";
+import "./copyright.scss"
+
+const Copyright = () => {
+    return(
+        <>
+            <div className="home-container">
+                <div className="title-row more-margin">
+                    <h1 className="title">Copyright</h1>
+                </div>
+                <div className="mode-space">
+                    <div className="mode-static">
+                        <a href="</a>">
+                            <img className="mode-img rounded" src={Rodrigo} alt="roro"/>
+                        </a>
+                            <p className="mode-text mt-text">Rodrigo Ramirez</p>
+                    </div>
+                    <div className="mode-static">
+                            <a href="https://github.com/cammarb">
+                                <img className="mode-img rounded" src={Camila} alt="cam"/>
+                            </a>
+                            <p className="mode-text mt-text">Camila Martínez</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Copyright;
